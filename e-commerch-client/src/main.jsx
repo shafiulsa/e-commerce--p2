eb4669c3bfeee956d6e292073f19e1d512dd1e83
shafiulsa <shafiulislam.sa@gamil.com>
2024-12-01 18:34:57 +0600

    first commit

---
+++ e-commerch-client/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+
+import { router } from './Routes/Routes.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  
+
+    <div className='max-w-screen-xl mx-auto'>
+
+      {/* <RouterProvider router={router} /> */}
+
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+
+
+    </div>
+  </StrictMode>,
+)
